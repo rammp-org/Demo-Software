f937c70e6a2a27594b1b53a7b84ad1ef9fa64f3a
Ariana <amb619@pitt.edu>
2026-03-19 11:48:58 -0400

    added comments

--- core/rammp_prototype_behavior/rammp_prototype_behavior/node_name_monitor.py
+++ core/rammp_prototype_behavior/rammp_prototype_behavior/node_name_monitor.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from ament_index_python.packages import get_package_share_directory
 import os
 import json
@@ -12,26 +11,27 @@ class NodeNameMonitor:
     def __init__(self, ros_node, callback):
         self.ros_node = ros_node
         self.callback = callback
-        self.ready = True
-        self.find_nodes_timer = self.ros_node.create_timer(
-            1.0, self.find_nodes_callback
-        )
 
-    def monitorNodes(self):
+    def NodesReady(self):
         actual_nodes = [
             name
             for name in self.get_fully_qualified_node_names()
-            if not name.startswith("_")
+            if not name.startswith(
+                "_"
+            )  # to remove built-in ros tools that show up by default when getting a nodes list
         ]
 
         with open(json_path) as f:
             data = json.load(f)
 
-        expected_nodes = [n["name"] for n in data["nodes"]]
+        expected_nodes = [
+            n["name"] for n in data["nodes"]
+        ]  # convert json dict format into list to compare with the actual nodes
 
         actual_nodes = set(
             actual_nodes
-        )  # make actual_nodes set instead of list to get O(n)
+        )  # make actual_nodes set instead of list to get O(n) time
+
         missing = [node for node in expected_nodes if node not in actual_nodes]
 
         if missing:

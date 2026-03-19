d43192465c2312b87aa167947d924d6f2cf9dd0b
ewang360 <elw283@pitt.edu>
2026-03-19 09:30:39 -0400

    fixed formatting for luci launch

--- core/rammp_prototype_bringup/launch/luci.launch.py
+++ core/rammp_prototype_bringup/launch/luci.launch.py
@@ -3,23 +3,25 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
-def generate_launch_description():
 
+def generate_launch_description():
     chair_ip_arg = DeclareLaunchArgument(
-        'chair_ip',
-        default_value='192.168.0.112', # IP address of HERL LUCI chair
-        description='IP address of the LUCI chair'
+        "chair_ip",
+        default_value="192.168.0.112",  # IP address of HERL LUCI chair
+        description="IP address of the LUCI chair",
     )
 
     luci_grpc_node = Node(
-        package='luci_grpc_interface',
-        executable='grpc_interface_node',
-        name='luci_grpc_interface_node',
-        arguments=['-a', LaunchConfiguration('chair_ip'), '--'],
-        output='screen'
+        package="luci_grpc_interface",
+        executable="grpc_interface_node",
+        name="luci_grpc_interface_node",
+        arguments=["-a", LaunchConfiguration("chair_ip"), "--"],
+        output="screen",
     )
 
-    return LaunchDescription([
-        chair_ip_arg,
-        luci_grpc_node,
-    ])
+    return LaunchDescription(
+        [
+            chair_ip_arg,
+            luci_grpc_node,
+        ]
+    )

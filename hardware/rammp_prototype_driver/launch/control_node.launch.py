e9f6585ac81e4be90806ef9d0b5538bb5f6edb3e
Ariana <amb619@pitt.edu>
2026-03-19 14:02:49 -0400

    added code for launch file and its installation in setup.py

---
+++ hardware/rammp_prototype_driver/launch/control_node.launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="rammp_prototype_driver",
+                executable="control_node",
+                name="MEBot_control_node",
+                output="screen",
+            )
+        ]
+    )

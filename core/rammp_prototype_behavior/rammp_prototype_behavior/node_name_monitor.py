bc2cd9a51d28738c3df8d6d55a359bfcdcec2b4f
Ariana <amb619@pitt.edu>
2026-03-19 11:44:23 -0400

    optimized node monitor logic

--- core/rammp_prototype_behavior/rammp_prototype_behavior/node_name_monitor.py
+++ core/rammp_prototype_behavior/rammp_prototype_behavior/node_name_monitor.py
@@ -1,62 +1,42 @@
 #!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 import json
 
 
-pkg_path = get_package_share_directory("rammp_pkg")
-json_path = os.path.join(pkg_path, "config", "nodes.json")
+pkg_path = get_package_share_directory("rammp_pkg")  # needs to be changed
+json_path = os.path.join(pkg_path, "config", "nodes.json")  # needs to be changed
 
 
-class FindNodes(Node):  # MODIFY NAME
-    def __init__(self, callback):
-        super().__init__("find_nodes")  # MODIFY NAME
+class NodeNameMonitor:
+    def __init__(self, ros_node, callback):
+        self.ros_node = ros_node
+        self.callback = callback
+        self.ready = True
+        self.find_nodes_timer = self.ros_node.create_timer(
+            1.0, self.find_nodes_callback
+        )
 
-        self.find_nodes_timer = self.create_timer(1.0, self.find_nodes_callback)
-
-    def find_nodes_callback(self):
+    def monitorNodes(self):
         actual_nodes = [
-            (name, ns)
-            for name, ns in self.get_node_names_and_namespaces()
+            name
+            for name in self.get_fully_qualified_node_names()
             if not name.startswith("_")
         ]
-
-        # for name, ns in actual_nodes:
-        #     self.get_logger().info(f"{name}, {ns}")
 
         with open(json_path) as f:
             data = json.load(f)
 
-        expected_nodes = [(n["name"], n["namespace"]) for n in data["nodes"]]
+        expected_nodes = [n["name"] for n in data["nodes"]]
 
-        for node in expected_nodes:
-            if node not in actual_nodes:
-                self.error()
+        actual_nodes = set(
+            actual_nodes
+        )  # make actual_nodes set instead of list to get O(n)
+        missing = [node for node in expected_nodes if node not in actual_nodes]
 
-        # if all expected node exist:
-        #  callback(True)
+        if missing:
+            self.callback(False)
+        else:
+            self.callback(True)
 
-        self.get_logger().info("SUCCESS")
-
-    def error(self):
-        self.get_logger().info("ERROR")
-
-    def is_ready() -> bool:
-        pass
-        # return self.is_ready
-
-
-def main(args=None):
-    def notice(ready: bool):
-        print("resutl: " + ready)
-
-    rclpy.init(args=args)
-    node = FindNodes(callback=notice)
-    rclpy.spin(node)
-    rclpy.shutdown()
-
-
-if __name__ == "__main__":
-    main()
+        return

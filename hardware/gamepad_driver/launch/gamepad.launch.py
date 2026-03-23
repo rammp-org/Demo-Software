1ae7ca2c760f632acab9aca0afc195102d443d0e
Ariana <amb619@pitt.edu>
2026-03-23 16:03:01 -0400

    added kinova launch descprition as a comment in launch file for gamepaddriver

--- hardware/gamepad_driver/launch/gamepad.launch.py
+++ hardware/gamepad_driver/launch/gamepad.launch.py
@@ -3,6 +3,21 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    # kortex_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(
+    #             get_package_share_directory("kortex_bringup"),
+    #             "launch",
+    #             "gen3.launch.py",
+    #         )
+    #     ),
+    #     launch_arguments={
+    #         "robot_ip": "192.168.1.10",
+    #         "dof": "6",
+    #         "gripper": "robotiq_2f_85",
+    #     }.items(),
+    # )
+
     return LaunchDescription(
         [
             Node(

21f304fdac1574a8368d7773686479a4b7f8bbcc
Ariana <amb619@pitt.edu>
2026-03-25 13:50:13 -0400

    restructured test action client to include feedback msgs

--- hardware/rammp_prototype_driver/rammp_prototype_driver/test_action_client.py
+++ hardware/rammp_prototype_driver/rammp_prototype_driver/test_action_client.py
@@ -16,14 +16,39 @@ class TestActionClient(Node):
         goal.direction = direction
 
         self.dummy_client.wait_for_server()
-        return self.dummy_client.send_goal_async(goal)
+        self.send_goal_future = self.dummy_client.send_goal_async(
+            goal, feedback_callback=self.feedback_callabck
+        )
+        self.send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal = future.result()
+        if not goal.accepted():
+            self.get_logger().info("Goal rejected")
+            return
+        self.get_logger().info("Goal accepted")
+
+        self.get_result_future = goal.get_result_async()
+        self.get_result_future.add_done_callback(self.result_callback)
+
+    def result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(str(result))
+        rclpy.shutdown()
+
+    def feedback_callabck(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info("Feedback: {0}".format(feedback))
+        pass
 
 
 def main(args=None):
     rclpy.init(args=args)
     action_client = TestActionClient()
-    future = action_client.send_goal(1)
-    rclpy.spin_until_future_complete(action_client, future)
+    # future = action_client.send_goal(1)
+    action_client.send_goal(1)
+    # rclpy.spin_until_future_complete(action_client, future)
+    rclpy.spin(action_client)
 
 
 if __name__ == "__main__":

605c21ddff33b09115ac55168b45e95b01ab1feb
ewang360 <elw283@pitt.edu>
2026-03-26 18:13:27 -0400

    changed encoder mappings

--- hardware/rammp_prototype_driver/firmware/Base/src/EncoderContainer/EncoderContainer.h
+++ hardware/rammp_prototype_driver/firmware/Base/src/EncoderContainer/EncoderContainer.h
@@ -8,16 +8,17 @@ public:
   // Position sensors pin selection
   // TODO: find out what this does
   float K_sensors = 1;
-  // TODO: switch enc1 and enc2 once they are switched back
-  Encoder Enc2 = Encoder(1, 0);    // RC bottom         (1,0)
-  Encoder Enc1 = Encoder(3, 2);    // RC top            (3,2)
+  // TODO: Enc9 and Enc8 are switched because ML drive wheel encoder wasn't
+  // working
+  Encoder Enc1 = Encoder(1, 0);    // RC bottom         (1,0)
+  Encoder Enc2 = Encoder(3, 2);    // RC top            (3,2)
   Encoder Enc3 = Encoder(5, 4);    // FC top            (5,4)
   Encoder Enc4 = Encoder(7, 6);    // FC bottom         (7,6)
   Encoder Enc5 = Encoder(9, 8);    // ML drive wheel    (9,8)
   Encoder Enc6 = Encoder(11, 10);  // ML front          (11,10)
   Encoder Enc7 = Encoder(24, 12);  // ML carriage       (24,12)
-  Encoder Enc8 = Encoder(26, 25);  // MR drive wheel    (26,25)
-  Encoder Enc9 = Encoder(28, 27);  // MR carriage       (28,27)
+  Encoder Enc9 = Encoder(26, 25);  // MR drive wheel    (26,25)
+  Encoder Enc8 = Encoder(28, 27);  // MR carriage       (28,27)
   Encoder Enc10 = Encoder(30, 29); // MR front          (30,29)
   Encoder Enc11 = Encoder(32, 31); // ML back           (32,31)
   Encoder Enc12 = Encoder(36, 37); // MR back           (36,37)

850977825e87b6640ecf1b684d9ddfa47b4d876d
ewang360 <elw283@pitt.edu>
2026-03-27 15:40:57 -0400

    fixed ament environment hook

--- core/rammp_prototype_bringup/setup.py
+++ core/rammp_prototype_bringup/setup.py
@@ -1,9 +1,16 @@
 import os
 from glob import glob
-
 from setuptools import find_packages, setup
 
 package_name = "rammp_prototype_bringup"
+pkg_dir = os.path.dirname(os.path.abspath(__file__))
+
+def local_glob(pattern):
+    orig = os.getcwd()
+    os.chdir(pkg_dir)
+    result = glob(pattern)
+    os.chdir(orig)
+    return result
 
 setup(
     name=package_name,
@@ -12,7 +19,9 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "launch"), local_glob("launch/*.py")),
+        (os.path.join("share", package_name, "environment"), local_glob("env_hooks/*.dsv")),
+        ("share/ament_index/resource_index/packages_with_environment_hooks", ["resource/" + package_name]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,

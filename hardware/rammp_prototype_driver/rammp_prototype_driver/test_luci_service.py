75e03797dd53de6db392212f4efbfec900d55d99
Ariana <amb619@pitt.edu>
2026-03-30 15:18:27 -0400

    implemented drive enable service clients and added and implemented test drive enable service client

--- hardware/rammp_prototype_driver/rammp_prototype_driver/test_luci_service.py
+++ hardware/rammp_prototype_driver/rammp_prototype_driver/test_luci_service.py
@@ -10,15 +10,16 @@ class TestLuciService(Node):  # MODIFY NAME
         super().__init__("test_luci_service")  # MODIFY NAME
         self.luci_req_client = self.create_client(SetBool, "drive_enable")
 
-    def send_req(self, req):
+    def send_req(self):
         req = SetBool.request()
+        req.data = True
         self.set_auto_remote_client.call_async(req)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = TestLuciService()  # MODIFY NAME
-    rclpy.spin(node)
+    rclpy.spin_once(node, timeout_sec=0.1)
     rclpy.shutdown()
 
 

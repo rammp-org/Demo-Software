e14b82a4722c8b93048b58c501e7a9d4fffc58d2
ewang360 <elw283@pitt.edu>
2026-03-30 09:40:10 -0400

    implemented encoder workaround

--- hardware/rammp_prototype_driver/firmware/Base/src/EncoderContainer/EncoderContainer.h
+++ hardware/rammp_prototype_driver/firmware/Base/src/EncoderContainer/EncoderContainer.h
@@ -9,16 +9,18 @@ public:
   // TODO: find out what this does
   float K_sensors = 1;
   // TODO: Enc9 and Enc8 are switched because ML drive wheel encoder wasn't
-  // working
+  // working (fixed?)
+  // TODO: Enc4 and Enc6 were switched because Enc4 connection seems to freeze
+  // loop
   Encoder Enc1 = Encoder(1, 0);    // RC bottom         (1,0)
   Encoder Enc2 = Encoder(3, 2);    // RC top            (3,2)
   Encoder Enc3 = Encoder(5, 4);    // FC top            (5,4)
-  Encoder Enc4 = Encoder(7, 6);    // FC bottom         (7,6)
+  Encoder Enc6 = Encoder(7, 6);    // FC bottom         (7,6)
   Encoder Enc5 = Encoder(9, 8);    // ML drive wheel    (9,8)
-  Encoder Enc6 = Encoder(11, 10);  // ML front          (11,10)
+  Encoder Enc4 = Encoder(11, 10);  // ML front          (11,10)
   Encoder Enc7 = Encoder(24, 12);  // ML carriage       (24,12)
-  Encoder Enc9 = Encoder(26, 25);  // MR drive wheel    (26,25)
-  Encoder Enc8 = Encoder(28, 27);  // MR carriage       (28,27)
+  Encoder Enc8 = Encoder(26, 25);  // MR drive wheel    (26,25)
+  Encoder Enc9 = Encoder(28, 27);  // MR carriage       (28,27)
   Encoder Enc10 = Encoder(30, 29); // MR front          (30,29)
   Encoder Enc11 = Encoder(32, 31); // ML back           (32,31)
   Encoder Enc12 = Encoder(36, 37); // MR back           (36,37)

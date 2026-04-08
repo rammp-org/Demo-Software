6c475c87e805af5f9c2669186436c394d26a6fc3
guo-max <gliyun@gmail.com>
2026-04-08 13:51:06 -0500

    Feature/27 gui monitor node (#85)
    * add GuiMonitor
    
    * WIP: able to control gripper using websocket
    
    * add unreal remote control websocket
    
    * update user input doc
    
    * add UserInput interface
    
    * update md file
    
    * update UserInputs msg
    
    * add function to set arm joints
    
    * WIP: add stream sender
    
    * fix set ui joints bug
    
    * fix bugs; sender can compile
    
    * mock arm driver
    
    * add warpper and redo the preset client
    
    * WIP: add node name monitor; add more arm functions
    
    * add cornell action clients
    
    * orgnize code
    
    * add test folder and move test code to it
    
    * add test folder and move test code to it
    
    * updated some state and transtions
    
    * add some trigger to action client
    
    * add mock drinking node
    
    * modify log message for debug
    
    * add mock open door
    
    * WIP: send request open door; have bug not response of goal request
    
    * call request open door from timer
    
    * bug fix for a deadlock
    
    * add mock funtion for drinking task as well
    
    * add mock for cup stabilizer
    
    * add mock arm retracted and home
    
    * clean code; add pause arm
    
    * resolve AI reviews
    
    * add mock nodes. add action client
    
    * add functions to control the base
    
    * mock test; bug fix
    
    * remove unused state
    
    * modify file names to use snake case
    
    * rename file
    
    * resovle AI review
    
    * update user inputs msg
    
    * add userinput srv; add chair joints functions
    
    * send syste state
    
    * update userInput; add function to set info
    
    * add user input callback
    
    * receive user input and send to Ros
    
    * link to service client
    
    * update log level
    
    * add system control subscriber to handle userinput; add publisher for system state
    
    * update for manual seat control
    
    * handle cancel and confirm
    
    * add camera subscribers; add functions to send camera data
    
    * bug fix; remove debug logs
    
    * add control for base drive enable
    
    * add the condition checks
    
    * send camera data to UI
    
    * add launch file
    
    * update folder name to user snake case
    
    * add curbinfo subscriber
    
    * add send mask function
    
    * update state machine to match UI flow
    
    * bug fix
    
    * update mocking functions
    
    * bug fix for cancel goal
    
    * fix other mock node for cancel goal
    
    * update streaming module
    
    * link open door to system control
    
    * try to fix requirements
    
    * use real node name instead of mock for openDoor
    
    * reverse AI's change
    
    * update requirements.txt to handle both x86_64 and aarch64
    
    * update open door mock to match the real implementation
    
    * add mock launch file; modify nav state
    
    * use home/retract trigger for homing cup; cup stabilizing could run after exit arm mode
    
    * fix typo that cause pause arm not work
    
    * allow cancel when orderdrink_holdingcup
    
    * update seat control publisher to use seatCommand msg
    
    * call setjoints if receive any jointState
    
    * send buttonInfo to UE
    
    * update camera stream sender to use group and role and stream id
    
    * update chair mock
    
    * update chair mock
    
    * send curb traverse progress to UI bridge
    
    * handle error state
    
    * update cornell interface and use it
    
    * send cupInfo to UI
    
    * update drink node name
    
    * normalize the bounding box
    
    * log user input timestamp
    
    * check is gripper open to determin holding drink or not
    
    * fix dependency
    
    * fix AI review

--- core/rammp_prototype_behavior/rammp_prototype_behavior/mocks/mock_opening_door.py
+++ core/rammp_prototype_behavior/rammp_prototype_behavior/mocks/mock_opening_door.py
@@ -4,47 +4,56 @@ import rclpy
 import rclpy.action
 import rclpy.node
 
-from rclpy.action import ActionServer, GoalResponse
+from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 from std_srvs.srv import SetBool
-from cmu_door_opener_interfaces.action import DoorOpenActionTypePlaceHolder
+from cmu_door_opener_interfaces.action import DoorOpen
+
+
+class ButtonDetector(rclpy.node.Node):
+    def __init__(self):
+        super().__init__("button_detector")
+        self.get_logger().info("Mock Button Detector Node has been started.")
+        self.cb_group = ReentrantCallbackGroup()
+
+        self._srv_detection_enable = self.create_service(
+            SetBool,
+            "/arm/door/detection/enable",
+            self._srv_detection_enable,
+            callback_group=self.cb_group,
+        )
+
+    def _srv_detection_enable(self, request, response):
+        if request.data:
+            self.get_logger().info("Button detection enabled.")
+        else:
+            self.get_logger().info("Button detection disabled.")
+        response.success = True
+        return response
 
 
 class OpenDoor(rclpy.node.Node):
     def __init__(self):
-        super().__init__("open_door_node")
+        super().__init__("button_push_controller")
         self.get_logger().info("Mock Opening Door Node has been started.")
         self._mock_action_reject = False  # default to accept all goals
         self._mock_action_result = True  # default to successful execution
         self._action_running = False
         self._action_counter = 0
         self.cb_group = ReentrantCallbackGroup()
-        self.create_service(
-            SetBool,
-            "/arm/door/detection/enable",
-            self._srv_detection_enable,
-            callback_group=self.cb_group,
-        )
         self._action_server = ActionServer(
             self,
-            DoorOpenActionTypePlaceHolder,
+            DoorOpen,
             "/arm/door/open",
             self._execute_callback,
             callback_group=self.cb_group,
             goal_callback=self._goal_callback,
+            cancel_callback=self._cancel_callback,
         )
 
-    def _srv_detection_enable(self, request, response):
-        if request.data:
-            self.get_logger().info("Door detection enabled.")
-        else:
-            self.get_logger().info("Door detection disabled.")
-        response.success = True
-        return response
-
     def publish_feedback(self, goal_handle) -> bool:
-        feedback = DoorOpenActionTypePlaceHolder.Feedback()
+        feedback = DoorOpen.Feedback()
         while self._action_counter > 0:
             self.get_logger().info("action counter left: " + str(self._action_counter))
             feedback.status = str(self._action_counter)
@@ -56,6 +65,10 @@ class OpenDoor(rclpy.node.Node):
             time.sleep(0.1)  # sleep 0.1s
         return True
 
+    def _cancel_callback(self, goal_handle):
+        self.get_logger().info("Received an action cancel request.")
+        return CancelResponse.ACCEPT
+
     def _execute_callback(self, goal_handle):
         self.get_logger().info("Received a  action goal.")
         if self._action_running or self._mock_action_reject:
@@ -63,16 +76,16 @@ class OpenDoor(rclpy.node.Node):
                 "Another action is already running. Rejecting new goal."
             )
             goal_handle.reject()
-            return DoorOpenActionTypePlaceHolder.Result()
+            return DoorOpen.Result()
         self._action_running = True
         self._action_counter = 20  # 2s action time.
         if not self.publish_feedback(goal_handle):  # canceled during action process
-            result = DoorOpenActionTypePlaceHolder.Result()
+            result = DoorOpen.Result()
             self._action_running = False
             return result
         self.get_logger().info("Action execution finished.")
         # mock result
-        result = DoorOpenActionTypePlaceHolder.Result()
+        result = DoorOpen.Result()
         self._action_running = False
 
         if self._mock_action_result:
@@ -94,14 +107,18 @@ def main(args=None):
     """Entry point for the mock opening door node."""
     rclpy.init(args=args)
     node = OpenDoor()
+    button_detector = ButtonDetector()
+
     executor = MultiThreadedExecutor()
     executor.add_node(node)
+    executor.add_node(button_detector)
     try:
         executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
         node.destroy_node()
+        button_detector.destroy_node()
         rclpy.shutdown()
 
 

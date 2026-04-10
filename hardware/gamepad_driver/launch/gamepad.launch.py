b61de4d8bbd0089f072cc1b0be0e4c0531eabae5
Ariana <amb619@pitt.edu>
2026-04-09 20:17:32 -0400

    removed arm driver and rammp prototype description from gamepad rdiver launch file

--- hardware/gamepad_driver/launch/gamepad.launch.py
+++ hardware/gamepad_driver/launch/gamepad.launch.py
@@ -1,32 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
-import os
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    description_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory("rammp_prototype_description"),
-                "launch",
-                "description.launch.py",
-            )
-        )
-    )
-
     return LaunchDescription(
         [
-            description_launch,
-            Node(
-                package="arm_driver",
-                executable="arm_driver",
-                name="arm_driver",
-                output="screen",
-            ),
             Node(
                 package="gamepad_driver",
                 executable="gamepad_node",

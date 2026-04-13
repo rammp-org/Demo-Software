9bbecf4613e2455f3543f27b20ea08d468e98feb
Ariana <amb619@pitt.edu>
2026-04-13 10:08:45 -0400

    changed estop node to use qos_profile to publish latest value to late joining subs and changed node to only publish upon state changes

--- hardware/xbox_controller_driver/xbox_controller_driver/estop_node.py
+++ hardware/xbox_controller_driver/xbox_controller_driver/estop_node.py
@@ -3,22 +3,27 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
 import keyboard
+from rclpy.qos import QoSProfile, DurabilityPolicy
 
 
 class EstopNode(Node):
     def __init__(self):
         super().__init__("estop_node")
 
-        self.estop_publisher = self.create_publisher(Bool, "estop", 10)
-        self.estop_timer = self.create_timer(1.0, self.estop_callback)
+        qos_profile = QoSProfile(depth=1, durability=DurabilityPolicy.TRANSIENT_LOCAL)
+
+        self.estop_publisher = self.create_publisher(Bool, "estop", qos_profile)
         self.estop_pressed = False
+
+        # upon init publish estop
+        msg = Bool()
+        msg.data = self.estop_pressed
+        self.estop_publisher.publish(msg)
 
         keyboard.add_hotkey("q", self._on_press)
 
     def _on_press(self):
         self.estop_pressed = not self.estop_pressed
-
-    def estop_callback(self):
         msg = Bool()
         msg.data = self.estop_pressed
         self.estop_publisher.publish(msg)

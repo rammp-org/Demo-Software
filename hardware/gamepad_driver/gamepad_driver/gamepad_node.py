be7a7ef575871f9dd38966fdaf6ba01cb2eb11f3
Liyun.guo <guo@assistivetech.dev>
2026-04-13 15:14:35 -0500

    update userInputs srv; send UserInputs when press xbox button

--- hardware/gamepad_driver/gamepad_driver/gamepad_node.py
+++ hardware/gamepad_driver/gamepad_driver/gamepad_node.py
@@ -1,3 +1,5 @@
+import threading
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -5,6 +7,7 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Vector3
 import tf2_ros
 from gui_interfaces.srv import UserInputs
+from rclpy.callback_groups import ReentrantCallbackGroup
 
 
 class gamepadNode(Node):
@@ -19,89 +22,99 @@ class gamepadNode(Node):
 
         self.last_button_state = [0] * 12
 
+        self.declare_parameter("home_button_index", 0)
+        self.home_button_index = (
+            self.get_parameter("home_button_index").get_parameter_value().integer_value
+        )
+        self.declare_parameter("manual_control_button_index", 1)
+        self.manual_control_button_index = (
+            self.get_parameter("manual_control_button_index")
+            .get_parameter_value()
+            .integer_value
+        )
+        self.declare_parameter("open_gripper_button_index", 4)
+        self.open_gripper_button_index = (
+            self.get_parameter("open_gripper_button_index")
+            .get_parameter_value()
+            .integer_value
+        )
+        self.declare_parameter("close_gripper_button_index", 5)
+        self.close_gripper_button_index = (
+            self.get_parameter("close_gripper_button_index")
+            .get_parameter_value()
+            .integer_value
+        )
+
+        # make sure the button indices are valid < 12 and not the same
+        button_indices = [
+            self.home_button_index,
+            self.manual_control_button_index,
+            self.open_gripper_button_index,
+            self.close_gripper_button_index,
+        ]
+        if any(index < 0 or index >= 12 for index in button_indices):
+            self.get_logger().error("Button indices must be between 0 and 11.")
+            raise ValueError("Button indices must be between 0 and 11.")
+        if len(set(button_indices)) != len(button_indices):
+            self.get_logger().error(
+                "Button indices for home, manual control, open gripper, and close gripper must be unique."
+            )
+            raise ValueError(
+                "Button indices for home, manual control, open gripper, and close gripper must be unique."
+            )
+
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
+        self._cb_group = ReentrantCallbackGroup()
         # Arm Velocity Publisher
-        self.twist_pub = self.create_publisher(Twist, "/arm/xbox/twist", 10)
+        self.twist_pub = self.create_publisher(
+            Twist, "/arm/xbox/twist", 10, callback_group=self._cb_group
+        )
 
         # UserInputs Service Client
         self.user_input_service_client = self.create_client(
             UserInputs, "/GuiBridge/user_input", callback_group=self._cb_group
         )
-        # self.home_pub = self.create_publisher(
-        #     JointTrajectory, "/joint_trajectory_controller/joint_trajectory", 10
-        # )
-
-        # self.open_gripper_client = self.create_client(Trigger, "/arm/open_gripper")
-        # self.close_gripper_client = self.create_client(Trigger, "/arm/close_gripper")
-
-        # self.preset_client = ActionClient(self, ReachPreset, "/arm/reach_preset")
-        # self.in_preset_mode = False
-
-        # self.manual_client = self.create_client(SetMode, "/arm/set_mode")
-
-    # def send_preset(self, value):
-    #     if self.in_preset_mode:
-    #         return
-    #     self.in_preset_mode = True
-
-    #     self.preset_client.wait_for_server()
-
-    #     goal_msg = ReachPreset.Goal()
-    #     goal_msg.preset = value
-
-    #     self.send_goal_future = self.preset_client.send_goal_async(
-    #         goal_msg, feedback_callback=self.feedback_callback
-    #     )
-
-    #     self.send_goal_future.add_done_callback(self.goal_response_callback)
-
-    # def goal_response_callback(self, future):
-    #     goal_handle = future.result()
-
-    #     if not goal_handle.accepted:
-    #         self.get_logger().info("Goal rejected")
-    #         self.in_preset_mode = False
-    #         self.send_manual_control_request()
-    #         return
-
-    #     self.get_logger().info("Goal accepeted")
-
-    #     self._get_result_future = goal_handle.get_result_async()
-    #     self._get_result_future.add_done_callback(self.result_callback)
-
-    # def feedback_callback(self, feedback_msg):
-    #     feedback = feedback_msg.feedback
-    #     self.get_logger().info(f"Feedback: {feedback.joint_states}")
-
-    # def result_callback(self, future):
-    #     result = future.result().result
-    #     self.get_logger().info(f"Result: {result.success}")
-    #     self.in_preset_mode = False
-    #     self.send_manual_control_request()
+        self.get_logger().info("gamepad Node initialized...")
 
     def openGripper(self):
-        # self.open_gripper_client.wait_for_service()
-
-        # request = Trigger.Request()
-        # future = self.open_gripper_client.call_async(request)
-        # future.add_done_callback(self.handle_service_response)
-        return
+        self.get_logger().info("Requesting gripper to open")
+        self.send_user_input(UserInputs.Request.ARM_OPEN_GRIPPER)
 
     def closeGripper(self):
-        return
+        self.get_logger().info("Requesting gripper to close")
+        self.send_user_input(UserInputs.Request.ARM_CLOSE_GRIPPER)
+
+    def send_user_input(self, input: str):
+        self.get_logger().info(f"Sending user input to ROS: {input}")
+        if self.user_input_service_client.wait_for_service(timeout_sec=1.0):
+            request = UserInputs.Request()
+            request.input = input
+            future = self.user_input_service_client.call_async(request)
+            event = threading.Event()
+            future.add_done_callback(lambda _: event.set())
+            event.wait(timeout=5.0)
+            if not future.done():
+                self.get_logger().error("User input service call timed out.")
+                return False
+            if future.result() is not None:
+                self.get_logger().info(f"User input '{input}' sent successfully.")
+                return future.result().success
+            else:
+                self.get_logger().error("User input service call failed.")
+                return False
+        else:
+            self.get_logger().error("User input service is not available.")
+            return False
 
     def send_manual_control_request(self):
-        return
-        # # Wait until service is available
-        # while not self.manual_client.wait_for_service():
-        #     self.get_logger().info("Service not available, waiting...")
+        self.get_logger().info("Requesting manual arm control on/off")
+        self.send_user_input(UserInputs.Request.ARM_MANUAL_ON)
 
-        # request = SetMode.Request()
-        # request.mode = 5
-        # future = self.manual_client.call_async(request)
-        # future.add_done_callback(self.handle_service_response)
+    def request_home(self):
+        self.get_logger().info("Requesting arm to move to home position")
+        self.send_user_input(UserInputs.Request.ARM_HOME)
 
     def joy_callback(self, msg):  # includes twist publishing
         try:
@@ -144,7 +157,9 @@ class gamepadNode(Node):
                 self.get_clock().now().nanoseconds / 1e9
             )  # convert to seconds
 
-            for button_index, preset_index in enumerate([0, 1, 2, 3]):
+            for button_index in enumerate(
+                [self.home_button_index, self.manual_control_button_index]
+            ):
                 if msg.buttons[button_index] == 1:
                     if self.last_button_state[button_index] == 0:
                         # Button just pressed — record the time
@@ -155,7 +170,10 @@ class gamepadNode(Node):
                             button_index, current_time
                         )
                         if press_duration >= self.hold_duration:
-                            self.send_preset(preset_index)
+                            if button_index == self.home_button_index:
+                                self.request_home()
+                            elif button_index == self.manual_control_button_index:
+                                self.send_manual_control_request()
                             self.button_press_time.pop(
                                 button_index
                             )  # reset so it doesn't fire repeatedly
@@ -164,9 +182,15 @@ class gamepadNode(Node):
                     self.button_press_time.pop(button_index, None)
 
             # --- Gripper Control (Buttons) ---
-            if msg.buttons[4] == 1 and self.last_button_state[4] == 0:
+            if (
+                msg.buttons[self.close_gripper_button_index] == 1
+                and self.last_button_state[self.close_gripper_button_index] == 0
+            ):
                 self.closeGripper()
-            elif msg.buttons[5] == 1 and self.last_button_state[5] == 0:
+            elif (
+                msg.buttons[self.open_gripper_button_index] == 1
+                and self.last_button_state[self.open_gripper_button_index] == 0
+            ):
                 self.openGripper()
 
             self.last_button_state = list(msg.buttons)

1a7156be5c77d85ec69b865160ee9d213f6d4b5d
guo-max <gliyun@gmail.com>
2026-04-16 18:26:26 -0500

    Bug/187 curb detection integration (#188)
    * Typo fix, lowercase curb info fields
    
    * enable curb descent detection
    
    * update mock to have descent detection node
    
    * curb info unit convertion
    
    * update descend curbInfo topic to use the same one as ascend;
    
    * use multi thread executor for Gui Bridge
    
    * Revert "update descend curbInfo topic to use the same one as ascend;"
    
    This reverts commit 936c9440bf64a05cc1dcc46abebaaed38d47362b.
    
    * Revert "Revert "update descend curbInfo topic to use the same one as ascend;""
    
    This reverts commit 66cb0fe3eb8b1b902565c25aa43689be5baae408.
    
    * Revert "use multi thread executor for Gui Bridge"
    
    This reverts commit 9e6d1ac3ad19704c511dc033c8cd74a320b94296.
    
    * try to add multi thread to Gui Bridge
    
    * fix transition when gripper open/close
    
    * fix gripper open close angle value
    
    * mock open/close gripper
    
    * remove arm state preset_motion; modify system control accordingly;
    
    * fix divid by 0 issue
    
    * Shorten image queues, correct joint transform
    
    ---------
    
    Co-authored-by: Swapnil Pande <swapnil.pande98@gmail.com>
    Co-authored-by: ewang360 <elw283@pitt.edu>

--- core/rammp_prototype_behavior/rammp_prototype_behavior/mocks/mock_curb_detection.py
+++ core/rammp_prototype_behavior/rammp_prototype_behavior/mocks/mock_curb_detection.py
@@ -32,18 +32,49 @@ class PerceptionCurbDetectionNode(rclpy.node.Node):
         return response
 
 
+class PerceptionCurbDescentDetectionNode(rclpy.node.Node):
+    def __init__(self):
+        super().__init__("perception_curb_descent_detection_node")
+        self.get_logger().info(
+            "Mock Perception Curb Descent Detection Node has been started."
+        )
+        self._mock_action_reject = False  # default to accept all goals
+        self._mock_action_result = True  # default to successful execution
+        self._action_running = False
+        self._action_counter = 0
+        self.cb_group = ReentrantCallbackGroup()
+        self.create_service(
+            SetBool,
+            "/nav/curb_descent/detect",
+            self._srv_detection_enable,
+            callback_group=self.cb_group,
+        )
+
+    def _srv_detection_enable(self, request, response):
+        if request.data:
+            self.get_logger().info("Curb descent detection enabled.")
+        else:
+            self.get_logger().info("Curb descent detection disabled.")
+        response.success = True
+        return response
+
+
 def main(args=None):
     """Entry point for the mock curb detection node."""
     rclpy.init(args=args)
     node = PerceptionCurbDetectionNode()
+    node_descent = PerceptionCurbDescentDetectionNode()
     executor = MultiThreadedExecutor()
     executor.add_node(node)
+    executor.add_node(node_descent)
+
     try:
         executor.spin()
     except KeyboardInterrupt:
         pass
     finally:
         node.destroy_node()
+        node_descent.destroy_node()
         rclpy.shutdown()
 
 

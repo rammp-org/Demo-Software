b6b7354f8381977f6a77f84a93dc99eff01db76d
ewang360 <elw283@pitt.edu>
2026-04-18 15:46:24 -0400

    configurations for running nodes

--- core/rammp_prototype_behavior/launch/mock.launch.py
+++ core/rammp_prototype_behavior/launch/mock.launch.py
@@ -1,5 +1,4 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -10,30 +9,30 @@ def generate_launch_description():
             #     executable="mock_arm_driver",
             #     output="screen",
             # ),
-            Node(
-                package="rammp_prototype_behavior",
-                executable="mock_drinking_node",
-                output="screen",
-            ),
-            Node(
-                package="rammp_prototype_behavior",
-                executable="mock_opening_door",
-                output="screen",
-            ),
-            Node(
-                package="rammp_prototype_behavior",
-                executable="mock_cup_stabilizer",
-                output="screen",
-            ),
+            #    Node(
+            #        package="rammp_prototype_behavior",
+            #        executable="mock_drinking_node",
+            #        output="screen",
+            #    ),
+            #            Node(
+            #                package="rammp_prototype_behavior",
+            #                executable="mock_opening_door",
+            #                output="screen",
+            #            ),
+            # Node(
+            #     package="rammp_prototype_behavior",
+            #     executable="mock_cup_stabilizer",
+            #     output="screen",
+            # ),
             # Node(
             #     package="rammp_prototype_behavior",
             #     executable="mock_chair_control",
             #     output="screen",
             # ),
-            Node(
-                package="rammp_prototype_behavior",
-                executable="mock_curb_detection",
-                output="screen",
-            ),
+            # Node(
+            #     package="rammp_prototype_behavior",
+            #     executable="mock_curb_detection",
+            #     output="screen",
+            # ),
         ]
     )

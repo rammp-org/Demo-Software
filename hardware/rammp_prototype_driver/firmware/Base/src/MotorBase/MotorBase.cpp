23c0b35ba39062b5eab177a50e7e762365aa05be
Ariana <amb619@pitt.edu>
2026-05-20 09:44:06 -0400

    created base motor class and changed motor class to be a subclass of motorbase

---
+++ hardware/rammp_prototype_driver/firmware/Base/src/MotorBase/MotorBase.cpp
@@ -0,0 +1,109 @@
+#include "MotorBase.h"
+
+MotorBase::MotorBase()
+    : pos_pid(0.0f, 0.0f, 0.0f, 0.0f, -10000.0f, 10000.0f, 1),
+      vel_pid(0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 1.0f, 10000) {}
+
+void MotorBase::updateSensorData(float current_pos, float dt) {
+  // Multiply by encoder direction to allow reversing logical sensor axis
+  float raw_pos = current_pos * encoder_dir;
+
+  // position input LPF
+  this->current_pos =
+      this->current_pos + this->lpf_input_alpha * (raw_pos - this->current_pos);
+
+  if (dt > 0.0f) {
+    float raw_vel = (this->current_pos - this->prev_pos) / dt;
+    // velocity input LPF
+    this->current_vel = this->current_vel +
+                        this->lpf_input_alpha * (raw_vel - this->current_vel);
+  }
+  this->prev_pos = this->current_pos;
+}
+
+float MotorBase::update(float dt) {
+  if (dt <= 0.0f)
+    return 0.0f;
+
+  switch (mode) {
+  case DISABLED:
+    target_pwm = 0.0f;
+    return 0.0f;
+
+  case POSITION_CONTROL:
+    if (limits_enabled) {
+      if (target_pos < pos_limit_min)
+        target_pos = pos_limit_min;
+      else if (target_pos > pos_limit_max)
+        target_pos = pos_limit_max;
+    }
+    target_vel = pos_pid.compute(target_pos, current_pos, dt);
+    // Fallthrough to velocity control
+
+  case VELOCITY_CONTROL:
+    target_pwm = vel_pid.compute(target_vel, current_vel, dt);
+    // Fallthrough to PWM output
+
+  case OPEN_LOOP:
+  default:
+    if (limits_enabled) {
+      if (current_pos <= pos_limit_min && target_pwm < 0) {
+        target_pwm = 0.0f;
+        vel_pid.reset();
+      } else if (current_pos >= pos_limit_max && target_pwm > 0) {
+        target_pwm = 0.0f;
+        vel_pid.reset();
+      }
+    }
+
+    // Apply direction multiplier to final PWM output
+    scaled_target_pwm = (int16_t)constrain(
+        target_pwm * direction * this->PWM_SCALE, -32767, 32767);
+    return scaled_target_pwm;
+  }
+}
+
+void MotorBase::initPIDs(float p_kp, float p_ki, float p_kd, float p_min,
+                         float p_max, float v_kp, float v_ki, float v_kd,
+                         float v_min, float v_max) {
+  pos_pid.setGains(p_kp, p_ki, p_kd);
+  pos_pid.setOutputLimits(p_min, p_max);
+  vel_pid.setGains(v_kp, v_ki, v_kd);
+  vel_pid.setOutputLimits(v_min, v_max);
+}
+
+void MotorBase::setTargetPosition(float pos) { target_pos = pos; }
+void MotorBase::setTargetVelocity(float vel) { target_vel = vel; }
+void MotorBase::setTargetPWM(float pwm) { target_pwm = pwm; }
+
+void MotorBase::attachStrainGauge(StrainGauge *sg) { _strain_gauge = sg; }
+
+void MotorBase::updateLoad() {
+  if (_strain_gauge != nullptr) {
+    current_load = _strain_gauge->getValue();
+  }
+}
+
+void MotorBase::setDirection(int8_t dir) { direction = (dir >= 0) ? 1 : -1; }
+void MotorBase::toggleDirection() { direction = -direction; }
+int8_t MotorBase::getDirection() const { return direction; }
+
+void MotorBase::setEncoderDirection(int8_t dir) {
+  encoder_dir = (dir >= 0) ? 1 : -1;
+}
+void MotorBase::toggleEncoderDirection() { encoder_dir = -encoder_dir; }
+int8_t MotorBase::getEncoderDirection() const { return encoder_dir; }
+
+void MotorBase::setInputLpfAlpha(float alpha) {
+  if (alpha < 0.0f)
+    alpha = 0.0f;
+  if (alpha > 1.0f)
+    alpha = 1.0f;
+  lpf_input_alpha = alpha;
+}
+
+void MotorBase::updateLimits(int32_t min, int32_t max) {
+  pos_limit_min = min;
+  pos_limit_max = max;
+  limits_enabled = (min != max);
+}
---
+++ hardware/rammp_prototype_driver/firmware/Base/src/MotorBase/MotorBase.h
@@ -0,0 +1,74 @@
+#ifndef MOTOR_BASE_H
+#define MOTOR_BASE_H
+
+#include "../PIDController/PIDController.h"
+#include "../StrainGauge/StrainGauge.h"
+#include <Arduino.h>
+
+class MotorBase {
+public:
+  enum ControlMode { DISABLED, OPEN_LOOP, VELOCITY_CONTROL, POSITION_CONTROL };
+
+  MotorBase();
+  virtual ~MotorBase() = default;
+
+  // --- Pure virtual: subclasses MUST define ---
+  virtual void setMode(ControlMode mode) = 0;
+  virtual void disable() = 0;
+
+  // --- Shared implementations ---
+  virtual void updateSensorData(float current_pos, float dt);
+  virtual float update(float dt);
+
+  virtual void initPIDs(float p_kp, float p_ki, float p_kd, float p_min,
+                        float p_max, float v_kp, float v_ki, float v_kd,
+                        float v_min, float v_max);
+
+  virtual void setTargetPosition(float target_pos);
+  virtual void setTargetVelocity(float target_vel);
+  virtual void setTargetPWM(float target_pwm);
+
+  virtual void attachStrainGauge(StrainGauge *sg);
+  virtual void updateLoad();
+
+  virtual void setDirection(int8_t dir);
+  virtual void toggleDirection();
+  virtual int8_t getDirection() const;
+
+  virtual void setEncoderDirection(int8_t dir);
+  virtual void toggleEncoderDirection();
+  virtual int8_t getEncoderDirection() const;
+
+  virtual void setInputLpfAlpha(float alpha);
+  virtual void updateLimits(int32_t min, int32_t max);
+
+  // --- State (public for direct access from control loops) ---
+  float current_pos = 0.0f;
+  float current_vel = 0.0f;
+  float prev_pos = 0.0f;
+  float prev_vel = 0.0f;
+  float target_pos = 0.0f;
+  float target_vel = 0.0f;
+  float target_pwm = 0.0f;
+  float scaled_target_pwm = 0.0f;
+  float current_load = 0.0f;
+  float lpf_input_alpha = 0.5f;
+
+  static constexpr float PWM_SCALE = 32767.0f;
+
+  int8_t direction = 1;
+  int8_t encoder_dir = 1;
+
+  int32_t pos_limit_min = 0;
+  int32_t pos_limit_max = 0;
+  bool limits_enabled = false;
+
+  PIDController pos_pid;
+  PIDController vel_pid;
+  ControlMode mode = DISABLED;
+
+protected:
+  StrainGauge *_strain_gauge = nullptr;
+};
+
+#endif

51d23d9eaff4b193dc1bec49b130796db651ae2b
Ariana <amb619@pitt.edu>
2026-07-02 10:01:27 -0400

    lowered target setting acceleration

--- hardware/rammp_prototype_driver/firmware/Base/src/HubMotor/HubMotor.cpp
+++ hardware/rammp_prototype_driver/firmware/Base/src/HubMotor/HubMotor.cpp
@@ -69,8 +69,8 @@ void HubMotor::writePWM() {
 
 void HubMotor::writeTargetPos() {
   int32_t pos = direction * this->target_pos * pos_scale;
-  int32_t spd = 20000;   // ERPM
-  int32_t accel = 60000; // ERPM/s
+  int32_t spd = 3000;   // was 20000
+  int32_t accel = 5000; // was 60000
   // Serial.print("DEBUG MSG: hub motor target position: ");
   // Serial.println(pos);
   uint32_t raw;
